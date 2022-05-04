8c68908f288d35baf57a524eda095f643fc28496
Adrian Sanabria-Diaz <sigmaenigma@users.noreply.github.com>
2022-05-03 23:14:28 -0500

    Update lightning_led.ino
    Formatting changes and cleaned up the code some

--- lightning_led.ino
+++ lightning_led.ino
@@ -1,18 +1,18 @@
 //
-//  FILE: lightning.pde
+//  FILE: lightning_led.ino
 //  AUTHOR: Rob Tillaart
 //  2nd Author: Adrian Sanabria-Diaz
 //  DATE: 2022-05-03
 //
 //  PURPOSE: simulate lighning POC
-//  
-//  Adrian: Added loop for multi LED support
+//
+//  Adrian: Added loop for multi LED support selected at random
 //
 #define BETWEEN 2579
-#define DURATION 43 
+#define DURATION 43
 #define TIMES 7
 
-const int myPWMArray [] = {3,5,6,9,10,11};
+const int myPWMArray [] = {3, 5, 6, 9, 10, 11};
 
 unsigned long lastTime = 0;
 int waitTime = 0;
@@ -21,10 +21,10 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println("\nStarting Up...");
-  
+
   // Loop through PWM Pins in the myPWMArray
   for (int thisPin : myPWMArray) {
-    pinMode(thisPin, OUTPUT); 
+    pinMode(thisPin, OUTPUT);
   }
 }
 
@@ -32,24 +32,21 @@ void loop()
 {
   if (millis() - waitTime > lastTime)  // time for a new flash
   {
-    // adjust timing params
+    // Adjust timing parameters
     lastTime += waitTime;
     waitTime = random(BETWEEN);
-    
+
     // Loop through PWM Pins in the myPWMArray
     for (int thisPin : myPWMArray) {
-      for (int i=0; i< random(TIMES); i++)
-        {
-          //String spin = char(thisPin)
-//          Serial.println("Lightning Detected! Pin " + thisPin + " Detected" );
-          Serial.println(thisPin);
-          //  + millis()
-          digitalWrite(thisPin, HIGH);
-          delay(20 + random(DURATION));
-          digitalWrite(thisPin, LOW);
-          delay(10);
-        }
+      for (int i = 0; i < random(TIMES); i++)
+      {
+        Serial.println(thisPin);
+        digitalWrite(thisPin, HIGH);
+        delay(100 + random(DURATION));
+        digitalWrite(thisPin, LOW);
+        delay(10);
       }
+    }
   }
   // do other stuff here
 }
